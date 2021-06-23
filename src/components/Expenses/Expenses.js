a844a7def349c2c387e0414c2062e6a028c17c93
Ïvan Hrushevich <ivan.grushevich1@gmail.com>
2021-06-23 10:28:38 +0300

    implement ExpenseList component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 
 import './Expenses.css';
@@ -24,14 +24,7 @@ const Expenses = props => {
 				selected={filteredYear}
 				onChangeFilter={filterChangeHandler}
 			/>
-			{filteredExpenses.map(expense => (
-				<ExpenseItem
-					key={expense.id}
-					title={expense.title}
-					amount={expense.amount}
-					date={expense.date}
-				/>
-			))}
+			<ExpensesList items={filteredExpenses} />
 		</Card>
 	);
 };
