0b83a9161c064d1765589bc0ee8c06d1bc4ffd76
Ïvan Hrushevich <ivan.grushevich1@gmail.com>
2021-06-23 10:11:58 +0300

    implement items filtering by date

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,14 +14,19 @@ const Expenses = props => {
 		setFilteredYear(selectedYear);
 	};
 
+	const filteredExpenses = props.items.filter(
+		expense => String(expense.date.getFullYear()) === filteredYear
+	);
+
 	return (
 		<Card className="expenses">
 			<ExpensesFilter
 				selected={filteredYear}
 				onChangeFilter={filterChangeHandler}
 			/>
-			{props.items.map(expense => (
+			{filteredExpenses.map(expense => (
 				<ExpenseItem
+					key={expense.id}
 					title={expense.title}
 					amount={expense.amount}
 					date={expense.date}
